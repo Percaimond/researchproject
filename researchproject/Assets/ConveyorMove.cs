ab8a9868652a2b420fe4aabff9b4677af69c0751
Daniel <danielweissenborn@outlook.de>
2021-06-15 16:33:56 +0200

    first conveyor working

---
+++ researchproject/Assets/ConveyorMove.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConveyorMove : MonoBehaviour
+{
+
+    public GameObject belt;
+    public Transform endpoint;
+    public float speed;
+    // Start is called before the first frame update
+    void OnTriggerStay(Collider other)
+	{
+        other.transform.position = Vector3.MoveTowards(other.transform.position, endpoint.position, speed * Time.deltaTime);
+	}
+}
